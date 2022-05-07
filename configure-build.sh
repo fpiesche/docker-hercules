507da44d11cf96983b12b6ebc4d9b94c5ed54e8e
fpiesche <florian@yellowkeycard.net>
2022-05-07 07:47:46 +0100

    Fix armv6 patch

--- configure-build.sh
+++ configure-build.sh
@@ -2,7 +2,7 @@
 
 source ${WORKSPACE}/.buildenv
 
-if [[ ${PLATFORM} == "armel"} ]]; then
+if [[ ${PLATFORM} == "armv6l"} ]]; then
    echo "=== Building on ARMv6 - patching ARM version detection to warn to make builds work"
    sed -i.bak -e "s/#error Target platform currently not supported/#warning Target platform currently not supported/" $HERCULES_SRC/src/common/atomic.h
 fi
