b9f6db77d01c56bf44ba6a3f8ebec1e3ac44c304
fpiesche <florian@yellowkeycard.net>
2022-05-07 02:32:29 +0100

    Patch atomic.h on armv6 to fix builds

--- configure-build.sh
+++ configure-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 source ${WORKSPACE}/.buildenv
+
+if [[ ${PLATFORM} == "armel"} ]]; then
+   echo "=== Building on ARMv6 - patching ARM version detection to warn to make builds work"
+   sed -i.bak -e "s/#error Target platform currently not supported/#warning Target platform currently not supported/" $HERCULES_SRC/src/common/atomic.h
+fi
+
 cd ${HERCULES_SRC}
 ./configure ${HERCULES_BUILD_OPTS} ${MEMORY_MANAGER_PARAM} ${SERVER_MODE_PARAM} ${PACKETVER_PARAM}
